1826a0f78dbbab00368cf534fb62fd111c7e3215
Syvrus <btcorbita@gmail.com>
2021-03-07 18:10:46 -0800

    Create main.cpp
    Established basic prompt UI

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <stdio>
+#include <string>
+
+int main(){
+    
+    string test;
+
+    cout << "Welcome to Brian Corbita's Feature Selection Algorithm\n"
+    cout << "Enter the name of the test file: ";
+
+    cin >> test;
+
+    cout << "Type the number of algorithm desired: \n";
+    cout << "1. Forward Selection\n";
+    cout << "2. Backward Elimination\n";
+
+    
+
+}
